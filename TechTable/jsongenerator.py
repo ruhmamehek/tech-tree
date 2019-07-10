9a66e5bc1523d466a1031e61b7a9653c843aa9e0
Bhavya <bhavya18333@iiitd.ac.in>
2019-07-11 00:42:20 +0530

    modified website appearance, added dynamic scroll to top, added credits

--- TechTable/jsongenerator.py
+++ TechTable/jsongenerator.py
@@ -1,6 +1,25 @@
+'''
+import pandas as pd
+
+df = pd.read_csv('sampleCourses.csv', dtype={
+    "Course Name" : str,
+    "Course Acronym" : str,
+    "Course Code" : str,
+    "Prerequisites" : str,
+    "Antirequisites" : str,
+    "Semester" : str,
+    "Preferable Prerequisites" : str,
+    "Professor" : str,
+    "Credits" : str,
+    "Link" : str
+ })
+
+#print(df)
+Export = df.to_json (r'.\sampleCourses.json', orient='records')
+'''
 import csv , json
 
-csvFilePath = "./sampleCourses.csv"
+csvFilePath = "./TechTreeCourses.csv"
 jsonFilePath = "./sampleCourses.json"
 arr = []
 
@@ -8,6 +27,7 @@ arr = []
 with open (csvFilePath) as csvFile:
 	csvReader = csv.DictReader(csvFile)
 	for csvRow in csvReader:
+		#print(csvRow)
 		arr.append(csvRow)
 
 # write the data to a json file
@@ -15,4 +35,3 @@ with open(jsonFilePath, "w") as jsonFile:
 	jsonFile.write('{"data": ')
 	jsonFile.write(json.dumps(arr, indent = 4))
 	jsonFile.write("}")
-
