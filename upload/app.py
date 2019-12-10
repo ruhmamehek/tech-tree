6a95ad23e9c6dae8cbcddb0a684e10888d92bcbc
ruhma-mehek <ruhma1836@iiitd.ac.in>
2019-12-11 01:34:18 +0530

    multiple file upload code

--- upload/app.py
+++ upload/app.py
@@ -5,4 +5,3 @@ UPLOAD_FOLDER = 'files/'
 app = Flask(__name__)
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
--- upload/main.py
+++ upload/main.py
@@ -17,22 +17,17 @@ def upload_form():
 @app.route('/', methods=['POST'])
 def upload_file():
 	if request.method == 'POST':
-        # check if the post request has the file part
-		if 'file' not in request.files:
+        # check if the post request has the files part
+		if 'files[]' not in request.files:
 			flash('No file part')
 			return redirect(request.url)
-		file = request.files['file']
-		if file.filename == '':
-			flash('No file selected for uploading')
-			return redirect(request.url)
-		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			flash('File successfully uploaded')
-			return redirect('/')
-		else:
-			flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
-			return redirect(request.url)
+		files = request.files.getlist('files[]')
+		for file in files:
+			if file and allowed_file(file.filename):
+				filename = secure_filename(file.filename)
+				file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		flash('File(s) successfully uploaded')
+		return redirect('/')
 
 if __name__ == "__main__":
     app.run()
